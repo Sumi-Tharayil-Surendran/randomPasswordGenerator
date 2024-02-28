628abbc9879e9dce451d8b9714274920f6a219fd
Sumi <sumi.tharayil@gmail.com>
2024-02-28 22:30:35 +0000

    Add result to input field in the function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  function generatePass(){
-    let words=['sumi','tharayil'];
+    let words=['sumi','tharayil','layali','neethu','rohini'];
     let numbers=[0,1,2,3,4,5,6,7,8,9]
     let symbols=["!","@","?","#","$","%","&","*",":"]
     let sentence = words[Math.floor(Math.random() *words.length)]
@@ -7,10 +7,12 @@
     let randomNum2 = numbers[Math.floor(Math.random() * numbers.length)]
     let symbol1 = symbols[Math.floor(Math.random() *symbols.length)]
     let symbol2 = symbols[Math.floor(Math.random() *symbols.length)]
-    console.log(sentence)
-    console.log(randomNum1)
-    console.log(randomNum2)
-    console.log(symbol1)
-    console.log(symbol2)
+    // console.log(sentence)
+    // console.log(randomNum1)
+    // console.log(randomNum2)
+    // console.log(symbol1)
+    // console.log(symbol2)
+    let txt = document.getElementById("txt");
+    txt.value = `${sentence}${randomNum1}${randomNum2}${symbol1}${symbol2}`;
  }
  generatePass()
